29280aadb51c8d2861694cbbda40b63a1883138e
Akshit Sethi <akshitsethi@gmail.com>
2020-12-29 18:02:45 +0530

    Make changes to how customer data is handled

--- includes/class-wc-customer-data-store-custom-table.php
+++ includes/class-wc-customer-data-store-custom-table.php
@@ -13,122 +13,6 @@
 class WC_Customer_Data_Store_Custom_Table extends WC_Customer_Data_Store {
 
 	/**
-	 * Gets the customers last order.
-	 *
-	 * @global $wpdb
-	 *
-	 * @param WC_Customer $customer The WC_Customer object, passed by reference.
-	 *
-	 * @return WC_Order|false The last order from this customer, or FALSE if the customer has not
-	 *                        made an order.
-	 */
-	public function get_last_order( &$customer ) {
-		global $wpdb;
-
-		$table      = wc_custom_order_table()->get_table_name();
-		$statuses   = wc_get_order_statuses();
-		$last_order = $wpdb->get_var(
-			$wpdb->prepare(
-				"
-				SELECT posts.ID FROM $wpdb->posts AS posts
-				LEFT JOIN " . esc_sql( $table ) . " AS meta on posts.ID = meta.order_id
-				WHERE meta.customer_id = %d
-				AND   posts.post_type  = 'shop_order'
-				AND   posts.post_status IN (" . implode( ', ', array_fill( 0, count( $statuses ), '%s' ) ) . ')
-				ORDER BY posts.ID DESC LIMIT 1',
-				array_merge( array( $customer->get_id() ), array_keys( $statuses ) )
-			)
-		);
-
-		return $last_order ? wc_get_order( (int) $last_order ) : false;
-	}
-
-	/**
-	 * Return the number of orders this customer has.
-	 *
-	 * @global $wpdb
-	 *
-	 * @param WC_Customer $customer The WC_Customer object, passed by reference.
-	 *
-	 * @return int The number of orders for this customer.
-	 */
-	public function get_order_count( &$customer ) {
-		global $wpdb;
-
-		$count = get_user_meta( $customer->get_id(), '_order_count', true );
-
-		if ( '' === $count ) {
-			$table    = wc_custom_order_table()->get_table_name();
-			$statuses = wc_get_order_statuses();
-			$count    = $wpdb->get_var(
-				$wpdb->prepare(
-					"
-					SELECT COUNT(*) FROM $wpdb->posts as posts
-					LEFT JOIN " . esc_sql( $table ) . " AS meta ON posts.ID = meta.order_id
-					WHERE meta.customer_id = %d
-					AND   posts.post_type  = 'shop_order'
-					AND   posts.post_status IN (" . implode( ', ', array_fill( 0, count( $statuses ), '%s' ) ) . ')',
-					array_merge( array( $customer->get_id() ), array_keys( $statuses ) )
-				)
-			);
-			update_user_meta( $customer->get_id(), '_order_count', $count );
-		}
-
-		return (int) $count;
-	}
-
-	/**
-	 * Return how much money this customer has spent.
-	 *
-	 * @global $wpdb
-	 *
-	 * @param WC_Customer $customer The WC_Customer object, passed by reference.
-	 *
-	 * @return float The total amount spent by the customer.
-	 */
-	public function get_total_spent( &$customer ) {
-		global $wpdb;
-
-		$spent = get_user_meta( $customer->get_id(), '_money_spent', true );
-
-		/**
-		 * Filter the total amount spent by the given customer across all orders.
-		 *
-		 * @param float $spent The total of all orders for this customer.
-		 * @param WC_Customer $customer The customer being queried.
-		 */
-		$spent = apply_filters( 'woocommerce_customer_get_total_spent', $spent, $customer );
-
-		// If there's no saved value, attempt to calculate one.
-		if ( '' === $spent ) {
-			$table    = wc_custom_order_table()->get_table_name();
-			$statuses = array_map( 'self::prefix_wc_status', wc_get_is_paid_statuses() );
-			$sql      = $wpdb->prepare(
-				"
-				SELECT SUM(meta.total) FROM $wpdb->posts as posts
-				LEFT JOIN " . esc_sql( $table ) . " AS meta ON posts.ID = meta.order_id
-				WHERE   meta.customer_id  = %d
-				AND     posts.post_type   = 'shop_order'
-				AND     posts.post_status IN (" . implode( ', ', array_fill( 0, count( $statuses ), '%s' ) ) . ')',
-				array_merge( array( $customer->get_id() ), $statuses )
-			);
-
-			/**
-			 * Filter the MySQL query used to determine how much a customer has spent across all orders.
-			 *
-			 * @param string      $sql      The prepared MySQL statement.
-			 * @param WC_Customer $customer The customer being queried.
-			 */
-			$sql   = apply_filters( 'woocommerce_customer_get_total_spent_query', $sql, $customer );
-			$spent = (float) $wpdb->get_var( $sql ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
-
-			update_user_meta( $customer->get_id(), '_money_spent', $spent );
-		}
-
-		return $spent;
-	}
-
-	/**
 	 * Register callbacks when the data store is first instantiated.
 	 */
 	public static function add_hooks() {
@@ -151,7 +35,7 @@ class WC_Customer_Data_Store_Custom_Table extends WC_Customer_Data_Store {
 	 * @param int    $user_id User ID to check.
 	 * @param int    $product_id Product ID to check.
 	 *
-	 * @return bool Whether or not the customer has already purchased the given product ID.
+	 * @return bool|null Whether or not the customer has already purchased the given product ID.
 	 */
 	public static function pre_customer_bought_product( $purchased, $customer_email, $user_id, $product_id ) {
 		global $wpdb;
@@ -178,7 +62,11 @@ class WC_Customer_Data_Store_Custom_Table extends WC_Customer_Data_Store {
 			$statuses      = array_map( 'self::prefix_wc_status', wc_get_is_paid_statuses() );
 
 			if ( 0 === count( $customer_data ) ) {
-				return false;
+				/**
+				 * Instead of returning false, we return null so that the function
+				 * moves on to query the wp_postmeta table for the same.
+				 */
+				return null;
 			}
 
 			$table  = wc_custom_order_table()->get_table_name();
